6815b8afcf61dd55c07b1d82430beaab4dd281d6
Zachary Kohs <zachary.c.kohs@gmail.com>
2023-08-31 11:30:11 -0400

    made DataFrame object and made line that would write it to a csv file

--- fruitsales.py
+++ fruitsales.py
@@ -1,4 +1,12 @@
 # import pandas
 import pandas as pd
 
+df_fruit_sales = pd.DataFrame({
+    'Apples': ['35', '21'],
+    'Bananas': ['41', '34']},
+    index=['2017 Sales', '2018 Sales']
+)
+
+df_fruit_sales.to_csv('fruit.csv')
+
 
